0458374b439291b6fef01df91ceea73292a89804
dibyaaaaax <dibya.gautam7@gmail.com>
2021-10-21 21:31:25 +0530

    Initial commit for solidity Smart Contract

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Vaccine-Passport-Verification
+
+Use https://www.dappuniversity.com/articles/blockchain-app-tutorial to set up the requirements.
---
+++ SmartContract/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var VaccinePassport = artifacts.require("./VaccinePassport.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(VaccinePassport);
+};
