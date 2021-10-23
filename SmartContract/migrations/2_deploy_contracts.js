4d61310728f21ad3f81ca239f268de28ba9567f7
dibyaaaaax <dibya.gautam7@gmail.com>
2021-10-23 13:12:22 +0530

    Add login contract

--- SmartContract/migrations/2_deploy_contracts.js
+++ SmartContract/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var VaccinePassport = artifacts.require("./VaccinePassport.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(VaccinePassport);
 };
