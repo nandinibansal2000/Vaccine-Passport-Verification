3ad5409275bf391f09e837e074eb7e06fbcc00f2
dibyaaaaax <dibya.gautam7@gmail.com>
2021-10-25 13:31:14 +0530

    Add prettier for sol files

--- SmartContract/contracts/VaccinePassport.sol
+++ SmartContract/contracts/VaccinePassport.sol
@@ -3,67 +3,76 @@
 pragma solidity ^0.8.0;
 
 contract VaccinePassport {
-  uint public vaxxpassCount;
+    uint256 public vaxxpassCount;
 
-  struct CardHolder {
-    string name;
-    string gender; // m/f/l/g/b/t/q
-    string dob; //dd-mm-yyyy
-    string aadharID;
-    string doseNo;
-  }
+    struct CardHolder {
+        string name;
+        string gender; // m/f/l/g/b/t/q
+        string dob; //dd-mm-yyyy
+        string aadharID;
+        string doseNo;
+    }
 
-  struct VaccineDetails {
-    string batchID;
-    string vaccineName;
-    string manufacturer;
-    string placeOfVaccination;
-  }
+    struct VaccineDetails {
+        string batchID;
+        string vaccineName;
+        string manufacturer;
+        string placeOfVaccination;
+    }
 
-  mapping(uint => VaccineDetails) public vaccineDetails;
+    mapping(uint256 => VaccineDetails) public vaccineDetails;
 
-  constructor() public {
+    constructor() public {
         addVaccineDetail(
-                        // card holder details
-                        "Dibya Gautam",
-                        "F",
-                        "24-12-1999",
-                        "1234325345",
-                        "2",
-                        //vacc details
-                        "2314",
-                        "Covishield",
-                        "Serum Institute",
-                        "XYZ Hospital, Delhi");
-      }
+            // card holder details
+            "Dibya Gautam",
+            "F",
+            "24-12-1999",
+            "1234325345",
+            "2",
+            //vacc details
+            "2314",
+            "Covishield",
+            "Serum Institute",
+            "XYZ Hospital, Delhi"
+        );
+    }
 
+    function addVaccineDetail(
+        string memory _name,
+        string memory _gender,
+        string memory _dob,
+        string memory _aadharID,
+        string memory _doseNo,
+        string memory _batchID,
+        string memory _vaccineName,
+        string memory _manufacturer,
+        string memory _placeOfVaccination
+    ) public {
+        VaccineDetails memory vaccineDetail = VaccineDetails(
+            _batchID,
+            _vaccineName,
+            _manufacturer,
+            _placeOfVaccination
+        );
+        CardHolder memory cardHolder = CardHolder(
+            _name,
+            _gender,
+            _dob,
+            _aadharID,
+            _doseNo
+        );
 
-  function addVaccineDetail(string memory _name,
-                            string memory _gender,
-                            string memory _dob,
-                            string memory _aadharID,
-                            string memory  _doseNo,
-                            string memory  _batchID,
-                            string memory _vaccineName,
-                            string memory _manufacturer,
-                            string memory _placeOfVaccination
-                            ) 
-    public {
-    VaccineDetails memory vaccineDetail = VaccineDetails(_batchID, _vaccineName, _manufacturer, _placeOfVaccination);
-    CardHolder memory cardHolder = CardHolder(_name, _gender, _dob, _aadharID, _doseNo);
-    
-    uint cardHolderID = vaxxpassCount++; // change this to SHA256 of cardHolder
+        uint256 cardHolderID = vaxxpassCount++; // change this to SHA256 of cardHolder
 
-    // string cardHolderID = string(sha256(abi.encode(
-    //     cardHolder.name,
-    //     cardHolder.gender,
-    //     cardHolder.dob,
-    //     cardHolder.aadharID,
-    //     cardHolder.doseNo
-    // )));
+        // string cardHolderID = string(sha256(abi.encode(
+        //     cardHolder.name,
+        //     cardHolder.gender,
+        //     cardHolder.dob,
+        //     cardHolder.aadharID,
+        //     cardHolder.doseNo
+        // )));
 
-    vaccineDetails[cardHolderID] = vaccineDetail;
-  }
-
-
+        vaccineDetails[cardHolderID] = vaccineDetail;
+    }
 }
