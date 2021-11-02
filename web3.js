2c2fbcabd9f34289b2504bd962ef67147a9265fe
Nandini Bansal <nandinibansal14@gmail.com>
2021-11-02 18:33:37 +0530

    Add web3 js code

---
+++ web3.js
@@ -0,0 +1,75 @@
+App = {
+  loading: false,
+  contracts: {},
+
+  load: async () => {
+    await App.loadWeb3();
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
+  },
+
+  loadWeb3: async () => {
+    if (typeof web3 !== "undefined") {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      window.alert("Please connect to Metamask.");
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        web3.eth.sendTransaction({
+          /* ... */
+        });
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider;
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      web3.eth.sendTransaction({
+        /* ... */
+      });
+    }
+    // Non-dapp browsers...
+    else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  },
+
+  loadAccount: async () => {
+    // Set the current blockchain account
+    App.account = web3.eth.accounts[0];
+  },
+
+  loadContract: async () => {
+    // Create a JavaScript version of the smart contract
+    const healthCareSignUp = await $.getJSON("HealthCareSignUp.json");
+    App.contracts.HealthCareSignUp = TruffleContract(healthCareSignUp);
+    App.contracts.HealthCareSignUp.setProvider(App.web3Provider);
+
+    // Hydrate the smart contract with values from the blockchain
+    App.healthCareSignUp = await App.HealthCareSignUp.TodoList.deployed();
+
+    const vaccinePassport = await $.getJSON("VaccinePassport.json");
+    App.contracts.VaccinePassport = TruffleContract(vaccinePassport);
+    App.contracts.VaccinePassport.setProvider(App.web3Provider);
+
+    // Hydrate the smart contract with values from the blockchain
+    App.vaccinePassport = await App.VaccinePassport.TodoList.deployed();
+  },
+
+  render:async() ={
+    
+  }
+};
