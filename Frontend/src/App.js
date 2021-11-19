209fd45a39dc1bc89d2a8ff51e31155c91710b1b
Pruthwiraj Nanda <pruthwiraj18075@iiitd.ac.in>
2021-11-20 01:18:40 +0530

    Create Signup/Login Feature

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Homepage from "./pages/Homepage";
+// import Healthcare from "./pages/Healthcare";
 
 function App() {
   return (
     <>
+      {/* <Healthcare></Healthcare> */}
       <Homepage></Homepage>
     </>
   );
